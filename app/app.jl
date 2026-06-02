27d1c3bec1e5abe7484a9f1f7d1ec3a661c879ca
jeffersonfparil <jeffersonparil@gmail.com>
2026-06-02 11:08:25 +1000

    testing basic barplot

--- app/app.jl
+++ app/app.jl
@@ -1,149 +1,135 @@
-using WGLMakie
+using WGLMakie, ColorSchemes
 using Bonito, Markdown
-using DataFrames, CSV, StatsBase
+using DataFrames, CSV, StatsBase, Distributions, Random
+using WordLists
 
-df = Da
+function simulate()
+    Random.seed!(6767)
+    years = string.("Year_", 2026:2031)
+    seasons = ["Early_Spring", "Late_Spring", "Summer", "Autumn", "Winter"]
+    harvests = ["Harvest_1", "Harvest_2", "Harvest_3"]
+    sites = lowercase.(rand(words("Dutch"), 5))
+    treatments = lowercase.(rand(words("German"), 3))
+    entries = lowercase.(rand(words("Spanish"), 25))
+    effects_year = rand(Chisq(5), length(years))
+    effects_season = [4, 5, 3, 2, 1]
+    effects_harvest = rand(Chisq(5), length(harvests))
+    effects_site = rand(Chisq(5), length(sites))
+    effects_treatment = rand(Chisq(5), length(treatments))
+    effects_entry = rand(Chisq(5), length(entries))
+    g_x_e = String[]
+    yield = Float64[]
+    for (i, year) in enumerate(years)
+        for (j, season) in enumerate(seasons)
+            for (k, harvest) in enumerate(harvests)
+                (harvest != harvests[1]) && (rand() < 0.5) ? continue : nothing
+                for (l, site) in enumerate(sites)
+                    for (m, treatment) in enumerate(treatments)
+                        for (n, entry) in enumerate(entries)
+                            # i=1; j=1; k=1; l=1; m=1; n=1; year = years[i]; season = seasons[j]; harvest = harvests[k]; site = sites[l]; treatment = treatments[m]; entry = entries[n];
+                            push!(g_x_e, join([year, season, harvest, site, treatment, entry], "\t"))
+                            push!(
+                                yield,
+                                effects_year[i] + 
+                                effects_season[j] + 
+                                effects_harvest[k] + 
+                                effects_site[l] + 
+                                effects_treatment[m] + 
+                                effects_entry[n] + 
+                                rand(Normal(0, 1))
+                            )
+                        end
+                    end
+                end
+            end
+        end
+    end
+    df = DataFrame(
+        years = [String(x[1]) for x in split.(g_x_e, "\t")],
+        seasons = [String(x[2]) for x in split.(g_x_e, "\t")],
+        harvests = [String(x[3]) for x in split.(g_x_e, "\t")],
+        sites = [String(x[4]) for x in split.(g_x_e, "\t")],
+        treatments = [String(x[5]) for x in split.(g_x_e, "\t")],
+        entries = [String(x[6]) for x in split.(g_x_e, "\t")],
+        yield = 5 .* yield,
+    )
+    df
+end
 
+df = simulate()
 
 WGLMakie.activate!(; use_html_widgets = true)
 
-fig = Figure(size=(1000, 500))
-gl = GridLayout(fig[2, 1], tellwidth = false)
-subgl = GridLayout(gl[1, 1])
+fig = Figure(size=(1_500, 900))
+layout = GridLayout(fig[1, 1], tellwidth = false)
 
-cb1 = Makie.Checkbox(subgl[1, 1], checked = false)
-cb2 = Makie.Checkbox(subgl[2, 1], checked = true)
-cb3 = Makie.Checkbox(subgl[3, 1], checked = true)
+year = unique(df.years)[1]
+site = unique(df.sites)[1]
+treatment = unique(df.treatments)[1]
+df_tmp = df |>
+    d -> filter(x -> (x.years == year) && (x.sites == site) && (x.treatments == treatment), d) |> 
+    d -> groupby(d, [:years, :seasons, :entries]) |>
+    d -> combine(d, "yield" => sum => "yield")
+df_tmp.x_entries = [findfirst(unique(df_tmp.entries) .== x) for x in df_tmp.entries]
+df_tmp.x_seasons = [findfirst(unique(df_tmp.seasons) .== x) for x in df_tmp.seasons]
+n = length(unique(df_tmp.entries))
+m = length(unique(df_tmp.seasons))
+colours_entries = resample(ColorSchemes.tol_bright, n)
+colours_seasons = resample(ColorSchemes.tol_sunset, m)
 
-Label(subgl[1, 2], "Dataset A", halign = :left)
-Label(subgl[2, 2], "Dataset B", halign = :left)
-Label(subgl[3, 2], "Dataset C", halign = :left)
-rowgap!(subgl, 8)
-colgap!(subgl, 8)
+axis_stacked_barplot = Axis(
+    layout[1,1],
+    ylabel = "Yield",
+    xticks = (1:n, unique(df_tmp.entries)),
+    xticklabelrotation = π/(1 + (75/n)),
+)
+barplot!(
+    axis_stacked_barplot,
+    df_tmp.x_entries,
+    df_tmp.yield,
+    stack = df_tmp.x_seasons,
+    color = colours_seasons[df_tmp.x_seasons],
+)
 
-ax1 = Axis(fig[1, 1])
+axis_dodged_barplot = Axis(
+    layout[1,2],
+    ylabel = "Yield",
+    xticks = (1:n, unique(df_tmp.entries)),
+    xticklabelrotation = π/(1 + (75/n)),
+)
+barplot!(
+    axis_dodged_barplot,
+    df_tmp.x_entries,
+    df_tmp.yield,
+    dodge = df_tmp.x_seasons,
+    color = colours_seasons[df_tmp.x_seasons],
+)
 
-for cb in [cb1, cb2, cb3]
-    lines!(ax1, cumsum(randn(1000)), alpha = @lift($(cb.checked) ? 1.0 : 0.1))
+axes = Dict()
+for (i, season) in enumerate(unique(df_tmp.seasons))
+    # i = 1; season = unique(df_tmp.seasons)[i]
+    row = Int(ceil((i+2) / 2))
+    col = ((i+1) % 2) + 1
+    # println("row: $row; col: $col")
+    axes[i] = Axis(
+        layout[row, col],
+        title = season,
+        ylabel = "Yield",
+        xticks = (1:n, unique(df_tmp.entries)),
+        xticklabelrotation = π/(1 + (75/n)),
+    )
+    idx = findall(df_tmp.seasons .== season)
+    barplot!(
+        axes[i],
+        df_tmp.x_entries[idx],
+        df_tmp.yield[idx],
+        color = colours_seasons[i],
+    )
 end
 
-ax2 = Axis(fig[1, 2])
-x = rand(100)
-y = x .* rand(100)
-scatter!(ax2, x, y)
+Legend(
+    layout[:,0],
+    [[MarkerElement(color=x, marker=:circle, markersize=15, strokecolor=:black)] for x in colours_seasons],
+    unique(df_tmp.seasons),
+)
 
-
-fig
-
-
-
-
-# Page() # for Franklin, you still need to configure
-# WGLMakie.activate!()
-# Makie.inline!(true) # Make sure to inline plots into Documenter output!
-# scatter(1:4, color=1:4)
-# N = 60
-# function xy_data(x, y)
-#     r = sqrt(x^2 + y^2)
-#     r == 0.0 ? 1f0 : (sin(r)/r)
-# end
-# l = range(-10, stop = 10, length = N)
-# z = Float32[xy_data(x, y) for x in l, y in l]
-# surface(
-#     -1..1, -1..1, z,
-#     colormap = :Spectral
-# )
-# using Observables
-
-# App() do session::Session
-#     n = 10
-#     index_slider = Slider(1:n)
-#     volume = rand(n, n, n)
-#     slice = map(index_slider) do idx
-#         return volume[:, :, idx]
-#     end
-#     fig = Figure()
-#     ax, cplot = contour(fig[1, 1], volume)
-#     rectplot = linesegments!(ax, Rect(-1, -1, 12, 12), linewidth=2, color=:red)
-#     on(index_slider) do idx
-#         translate!(rectplot, 0,0,idx)
-#     end
-#     heatmap(fig[1, 2], slice)
-#     slider = DOM.div("z-index: ", index_slider, index_slider.value)
-#     return Bonito.record_states(session, DOM.div(slider, fig))
-# end
-
-# App() do session
-#     f, ax, pl = scatter(1:4, markersize=100, color=Float32[0.3, 0.4, 0.5, 0.6])
-#     custom_info = ["a", "b", "c", "d"]
-#     on_click_callback = js"""(plot, index) => {
-#         // the plot object is currently just the raw THREEJS mesh
-#         console.log(plot)
-#         // Which can be used to extract e.g. position or color:
-#         const {pos, color} = plot.geometry.attributes
-#         console.log(pos)
-#         console.log(color)
-#         const x = pos.array[index*2] // everything is a flat array in JS
-#         const y = pos.array[index*2+1]
-#         const c = Math.round(color.array[index] * 10) / 10 // rounding to a digit in JS
-#         const custom = $(custom_info)[index]
-#         // return either a string, or an HTMLNode:
-#         return "Point: <" + x + ", " + y + ">, value: " + c + " custom: " + custom
-#     }
-#     """
-
-#     # ToolTip(figurelike, js_callback; plots=plots_you_want_to_hover)
-#     tooltip = WGLMakie.ToolTip(f, on_click_callback; plots=pl)
-#     return DOM.div(f, tooltip)
-# end
-
-# App() do session::Session
-#     # We can now use this wherever we want:
-#     fig = Figure(size=(300, 300))
-#     contour(fig[1,1], rand(4,4))
-#     card = Card(Grid(
-#         Centered(DOM.h1("Hello"); style=Styles("grid-column" => "1 / 3")),
-#         StylableSlider(1:100; style=Styles("grid-column" => "1 / 3")),
-#         DOM.img(src="https://julialang.org/assets/infra/logo.svg"),
-#         fig; columns="1fr 1fr", justify_items="stretch"
-#     ))
-#     # Markdown creates a DOM as well, and you can interpolate
-#     # arbitrary jsrender'able elements in there:
-#     return DOM.div(card)
-# end
-
-# spinner = WGLMakie.CircleSpinner(;
-#     size=100,                                    # diameter in pixels
-#     stroke=10,                                   # border width in pixels
-#     color="red",                       # color of the spinning part
-#     background_color="rgba(1, 0, 0, 0.9)",      # color of the background circle
-#     duration=1                                  # rotation speed in seconds
-# )
-# WGLMakie.activate!(; spinner=spinner)
-
-# WGLMakie.activate!(; use_html_widgets = true)
-
-# fig = Figure(size=(1000, 500))
-# ax = Axis(fig[1, 1])
-# ax2 = Axis(fig[1, 2])
-# sl = Makie.Slider(fig[2, 1], range = 0:0.1:10, startvalue = 5, tellwidth=false)
-# btn = Makie.Button(fig[3, 1], label = "Click me", tellwidth=false)
-# x = 0:10
-# lines!(ax, x, map(y -> sin.(y .* x), sl.value))
-
-# scatter!(ax2, 1:4, color=1:4)
-
-
-# fig # Will get rendered with HTML widgets
-
-
-# ########################################################
-# using GLMakie
-# f = Figure()
-
-# gl = GridLayout(f[2, 1], tellwidth = false)
-# subgl = GridLayout(gl[1, 1])
-
-# cb1 = Checkbox(subgl[1, 1], checked = false)
-# cb2 = Checkbox(subgl[2, 1], checked = true)
-# cb3 = Checkbox(subgl[3, 1], checked = true)
